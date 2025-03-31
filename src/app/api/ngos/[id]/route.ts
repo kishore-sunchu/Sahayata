8f2c484496a5ff0f68695ed6cd3272a1041f5205
kishore-sunchu <128731985+kishore-sunchu@users.noreply.github.com>
2025-03-31 16:28:00 +0530

    issue: api id fix

--- src/app/api/ngos/[id]/route.ts
+++ src/app/api/ngos/[id]/route.ts
@@ -1,12 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { NextResponse } from "next/server";
 import { connectToDatabase } from "@/lib/db";
 import Ngo from "@/models/Ngo";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: Request, { params }: { params: { id: string } }) {
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
   try {
     await connectToDatabase();
-    const { id } = await params
+    const id = (await params).id;
     const ngo = await Ngo.findById(id);
     if (!ngo) return NextResponse.json({ success: false, message: "NGO not found" }, { status: 404 });
 
@@ -16,11 +19,15 @@ export async function GET(req: Request, { params }: { params: { id: string } }) 
   }
 }
 
-export async function PATCH(req: Request, { params }: { params: { id: string } }) {
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
   try {
     await connectToDatabase();
-    const body = await req.json();
-    const ngo = await Ngo.findByIdAndUpdate(params.id, body, { new: true });
+    const id = (await params).id;
+    const body = await request.json();
+    const ngo = await Ngo.findByIdAndUpdate(id, body, { new: true });
     if (!ngo) return NextResponse.json({ success: false, message: "NGO not found" }, { status: 404 });
 
     return NextResponse.json({ success: true, data: ngo }, { status: 200 });
@@ -29,10 +36,14 @@ export async function PATCH(req: Request, { params }: { params: { id: string } }
   }
 }
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
   try {
     await connectToDatabase();
-    const ngo = await Ngo.findByIdAndDelete(params.id);
+    const id = (await params).id;
+    const ngo = await Ngo.findByIdAndDelete(id);
     if (!ngo) return NextResponse.json({ success: false, message: "NGO not found" }, { status: 404 });
 
     return NextResponse.json({ success: true, message: "NGO deleted" }, { status: 200 });
