07b7697211e410087f01a5abe7c87c2e2f0a18d6
kishore-sunchu <128731985+kishore-sunchu@users.noreply.github.com>
2025-04-03 15:52:58 +0530

    fix: not found page issue

---
+++ src/app/(error)/not-found/page.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <>
+            <h1 className="bg-primary text-5xl">404</h1>
+            <h1 className="bg-secondary text-2xl">Not Found</h1>
+        </>
+    )
+}
